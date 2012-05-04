897556119c757187b596474356a0e89b0ac7f45e
Matt Henry <greenideas@gmail.com>
2012-05-04 15:58:20 -0400

    wip make inline images into background images

--- app/toura/components/FeedItemDetail.js
+++ app/toura/components/FeedItemDetail.js
@@ -1,8 +1,10 @@
 dojo.provide('toura.components.FeedItemDetail');
 
+dojo.require('mulberry.ui.BackgroundImage');
 dojo.require('mulberry._Component');
 dojo.require('toura.components.VideoPlayer');
 dojo.require('dojo.date.locale');
+dojo.require('dojo.NodeList-manipulate');
 
 dojo.declare('toura.components.FeedItemDetail', mulberry._Component, {
   templateString : dojo.cache('toura.components', 'FeedItemDetail/FeedItemDetail.haml'),
@@ -71,11 +73,27 @@ dojo.declare('toura.components.FeedItemDetail', mulberry._Component, {
 
     dojo.attr(this.externalLink, 'href', this.item.link);
 
+    this._prepareImages();
     this._setupLinks();
 
     if (this.region) {
       this.region.refreshScroller();
     }
+  },
+
+  _prepareImages : function() {
+    dojo.forEach(this.domNode.querySelectorAll('img'), function(image){
+      dojo.query(image)
+          .wrap("<div class='replacedImage' dojotype=mulberry.ui.BackgroundImage imageUrl="+image.src+"></div>");
+
+      image.parentNode.removeChild(image);
+    }, this);
+
+    dojo.forEach(this.domNode.querySelectorAll('.replacedImage'), function(node){
+      var bgImg = new mulberry.ui.BackgroundImage(node);
+      console.log(bgImg);
+      bgImg.loadImage();
+    }, this);
   },
 
   _setupLinks : function() {
