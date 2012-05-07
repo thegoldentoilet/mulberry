739e6892647cdd3c51bd578882959507e51d8ff9
Chris Barrett <christophersbarrett@gmail.com>
2012-05-07 12:58:17 -0500

    Added a line to add rvm command to .bash_profile

--- install.sh
+++ install.sh
@@ -38,15 +38,16 @@ echo "Setting PATH..."
 DIR="$( pwd )"
 
 # Cram it in our bash_profile
-echo -e "\n\nexport PATH=\$PATH:${DIR}/cli/bin" >> ~/.bash_profile
+echo -e "[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function" >> ~/.bash_profile
+echo -e "\nexport PATH=\$PATH:${DIR}/cli/bin" >> ~/.bash_profile
 
 # Set it for this console session
 export PATH=$PATH:$DIR/cli/bin
 
 echo "Downloading Android packages and tools"
-android update sdk --no-ui -t tools,platform-tools,android-15,android-13,android-10,android-8,sysimg-15,addon-google_apis-google-15,addon-google_apis-google-13,addon-google_apis-google-10,addon-google_apis-google-8
+#android update sdk --no-ui -t tools,platform-tools,android-15,android-13,android-10,android-8,sysimg-15,addon-google_apis-google-15,addon-google_apis-google-13,addon-google_apis-google-10,addon-google_apis-google-8
 
 echo "Configuring builder/project_templates/android/local.properties"
-android update project -p builder/project_templates/android/
+#android update project -p builder/project_templates/android/
 
 echo "Please open README.md for steps to configure Android."
