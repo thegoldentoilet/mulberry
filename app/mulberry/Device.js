0246ab443ee36eba285e1ce9d4e5b289449c9f45
Evan Hensleigh <ehensleigh@toura.com>
2012-05-21 18:16:10 -0400

    Added a user agent sniffer to mulberry.Device. :-(

--- app/mulberry/Device.js
+++ app/mulberry/Device.js
@@ -1,9 +1,27 @@
 dojo.provide('mulberry.Device');
 
+dojo.require('mulberry.Utilities');
 dojo.require('mulberry.app.Config');
 
 mulberry._loadDeviceConfig = function() {
-
+  var browserDevices = {
+    'ios' : {
+      re : /(iPhone|iPod|iPad)/,
+      version: function(ua) {
+        var vnum = parseFloat(ua.match(/([\d_]+) like Mac OS X/)[1].replace('_', '.'));
+        if (!vnum) { return 1; }    // I am hoping this is right?
+        return vnum;
+      }
+    },
+    'android' : {
+      re : /(Android)/,
+      version: function(ua) {
+        return parseFloat(ua.match(/Android ([\d\.]+)/)[1]);
+      }
+    }
+  }, ua;
+  
+  
   function getDeviceType() {
     var body = dojo.body(),
         minDim = Math.min(body.offsetWidth, body.offsetHeight);
@@ -16,6 +34,18 @@ mulberry._loadDeviceConfig = function() {
     os : 'browser',
     standalone: !!navigator.standalone
   };
+  
+  if (mulberry.Device.os === 'browser') {
+    ua = window.navigator.userAgent;
+        
+    dojo.forIn(browserDevices, function(k, v) {
+      if (ua.search(v.re) > -1) { mulberry.Device.browserOS = k; }
+    });
+    
+    if (mulberry.Device.browserOS) {
+      mulberry.Device.browserVersion = browserDevices[mulberry.Device.browserOS].version(ua);
+    }
+  }
 
 };
 
