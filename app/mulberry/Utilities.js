06fcdfa1ca42247818469bd5da7bc5b579b2da10
Matt Henry <greenideas@gmail.com>
2012-05-29 13:28:49 -0400

    add check for hashchange event

--- app/mulberry/Utilities.js
+++ app/mulberry/Utilities.js
@@ -49,14 +49,17 @@ mulberry.util = {
      * Currently checking:
      * 1. That we're in a mobile web build (mulberry.Device.os is "browser").
      * 2. Support for webkit prefixes, which rules out non-webkit browsers.
-     * 3. Support for touch events. This rules out pre-Froyo Android.
+     * 3. Support for touch events.
+     * 4. Support for the hashchange event. This rules out pre-Froyo Android.
+     * 5. Support for Web SQL.
      * */
 
     var div = document.createElement("div"),
         supportsMulberry,
         supportsWebSql,
         supportsWebkitPrefixes,
-        supportsTouch;
+        supportsHashchange,
+        supportsFontFace;
 
     if (mulberry.Device.os != "browser") {
       return true;
@@ -69,9 +72,11 @@ mulberry.util = {
     supportsWebkitPrefixes = typeof div.style.webkitTransform !== "undefined";
     supportsTouch = 'ontouchstart' in document.documentElement;
     supportsWebSql = 'openDatabase' in window;
+    supportsHashchange = 'onhashchange' in window;
 
     supportsMulberry = supportsWebkitPrefixes &&
                        supportsTouch &&
+                       supportsHashchange &&
                        supportsWebSql;
 
     return supportsMulberry;
