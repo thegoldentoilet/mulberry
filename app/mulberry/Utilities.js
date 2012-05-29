1afc554d95753b09f3c435dfcc0555fdd1244ce5
Matt Henry <greenideas@gmail.com>
2012-05-29 14:09:48 -0400

    move browser support method to Device class

--- app/mulberry/Utilities.js
+++ app/mulberry/Utilities.js
@@ -42,44 +42,6 @@ mulberry.util = {
         fromStyles[style]
       );
     });
-  },
-
-  supportedBrowser : function() {
-    /*
-     * Currently checking:
-     * 1. That we're in a mobile web build (mulberry.Device.os is "browser").
-     * 2. Support for webkit prefixes, which rules out non-webkit browsers.
-     * 3. Support for touch events.
-     * 4. Support for the hashchange event. This rules out pre-Froyo Android.
-     * 5. Support for Web SQL.
-     * */
-
-    var div = document.createElement("div"),
-        supportsMulberry,
-        supportsWebSql,
-        supportsWebkitPrefixes,
-        supportsHashchange,
-        supportsFontFace;
-
-    if (mulberry.Device.os != "browser") {
-      return true;
-    }
-
-    if (window.location.hostname === "localhost") {
-      return true;
-    }
-
-    supportsWebkitPrefixes = typeof div.style.webkitTransform !== "undefined";
-    supportsTouch = 'ontouchstart' in document.documentElement;
-    supportsWebSql = 'openDatabase' in window;
-    supportsHashchange = 'onhashchange' in window;
-
-    supportsMulberry = supportsWebkitPrefixes &&
-                       supportsTouch &&
-                       supportsHashchange &&
-                       supportsWebSql;
-
-    return supportsMulberry;
   }
 };
 
