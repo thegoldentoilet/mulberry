89a21f26d30cec58bc3eff6ca822a8e4e0e8d777
Chris Barrett <christophersbarrett@gmail.com>
2012-06-01 17:20:11 -0500

    first non-working commit to resize body in AB

--- app/mulberry/base.js
+++ app/mulberry/base.js
@@ -2,7 +2,6 @@ dojo.provide('mulberry.base');
 
 $m = mulberry;
 mulberry.version = '0.3';
-
 dojo.require('mulberry.app.Config');
 
 dojo.require('mulberry.Device');
@@ -19,6 +18,11 @@ dojo.require('mulberry.app._base');
 
 dojo.requireLocalization('mulberry', 'mulberry');
 
+var fixPix = function() {
+  var pix = (window.outerHeight-54) + "px";        
+  dojo.style(document.body, 'height', pix);  
+};
+
 var readyFn = function() {
   // open up the database connection so we can work with it
   mulberry.app.DeviceStorage.init(mulberry.app.Config.get("id") || 'mulberry');
@@ -27,13 +31,24 @@ var readyFn = function() {
   dojo.publish('/app/deviceready');
 
   // bootstrapping process must publish this topic
-  dojo.subscribe('/app/ready', function() {
+  dojo.subscribe('/app/ready', function() {    
+    
     mulberry.app.Router.init();
-    mulberry.app.UI.hideSplash();
+    mulberry.app.UI.hideSplash();   
+    alert(mulberry.Device.os);
+    if(mulberry.Device.os === 'android'){
+      fixPix();
+      
+      dojo.connect("onorientationchange", function() {
+        setTimeout(function() {
+          fixPix();
+        }, 200);
+      });
+    }
   });
-
 };
 
 document.addEventListener("deviceready", function() {
   dojo.ready(readyFn);
 }, false);
+
