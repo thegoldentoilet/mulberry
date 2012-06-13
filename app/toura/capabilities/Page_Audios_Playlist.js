d698481c9e5638d51c9def4097e448ed0869927f
Evan Hensleigh <ehensleigh@toura.com>
2012-06-13 14:40:00 -0400

    Stub capability.

---
+++ app/toura/capabilities/Page_Audios_Playlist.js
@@ -0,0 +1,7 @@
+dojo.provide('toura.capabilities.Page_Audios_Playlist');
+
+dojo.require('mulberry._Capability');
+
+dojo.declare('toura.capabilities.Page_Audios_Playlist', mulberry._Capability, {
+
+});
