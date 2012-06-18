29807b87edc482e0df203ea4517828ac4a5afe2c
Evan Hensleigh <ehensleigh@toura.com>
2012-06-18 18:36:20 -0400

    Added backing click-catcher.

--- app/toura/capabilities/Page_Audios_Playlist.js
+++ app/toura/capabilities/Page_Audios_Playlist.js
@@ -9,17 +9,31 @@ dojo.declare('toura.capabilities.Page_Audios_Playlist', mulberry._Capability, {
   },
 
   connects : [
-    [ 'page', 'init', '_setup' ],
+    [ 'page', 'startup', '_setup' ],
     [ 'playlistButton', 'onClick', 'togglePlaylist' ]
   ],
   
+  coverDiv : null,
+
   init : function() {
     this.playlistVisible = false;
     this.audioList.region.hide();
   },
 
   _setup : function() {
-    // debugger;
+    // this is not ideal, since it sort of breaks the idea of components. But it
+    // will work until we get a proper solution for modal windows.
+    var contentRegion = dojo.query('.page-content')[0];
+    this.coverDiv = document.createElement('div');
+
+    dojo.addClass(this.coverDiv, ['cover', 'hide']);
+    dojo.connect(this.coverDiv, 'click', this, function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this._hidePlaylist();
+    });
+
+    contentRegion.appendChild(this.coverDiv);
   },
   
   togglePlaylist : function() {
@@ -34,14 +48,14 @@ dojo.declare('toura.capabilities.Page_Audios_Playlist', mulberry._Capability, {
     this.playlistVisible = true;
     this.playlistButton.addClass('playlist-visible');
     this.audioList.region.show();
+    dojo.removeClass(this.coverDiv, 'hide');
   },
 
   _hidePlaylist : function() {
     this.playlistVisible = false;
     this.playlistButton.removeClass('playlist-visible');
     this.audioList.region.hide();
-  },
-
-  commaStopper : undefined
+    dojo.addClass(this.coverDiv, 'hide');
+  }
   
 });
