d13546717c2d84e2343a347114a46da1e1ee2654
Evan Hensleigh <ehensleigh@toura.com>
2012-06-18 15:16:19 -0400

    Real styles in place (or at least a start on 'em).

--- app/toura/components/PlaylistButton.js
+++ app/toura/components/PlaylistButton.js
@@ -1,8 +1,8 @@
 dojo.provide('toura.components.PlaylistButton');
 
-dojo.require('mulberry._Component');
+dojo.require('toura.components.buttons._Button');
 
-dojo.declare('toura.components.PlaylistButton', mulberry._Component, {
+dojo.declare('toura.components.PlaylistButton', toura.components.buttons._Button, {
   templateString : dojo.cache('toura.components', 'PlaylistButton/PlaylistButton.haml'),
 
   onClick : function(e) {
