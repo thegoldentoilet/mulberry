85b0d65131fc4371d17167c137337a4b6dcedd3f
Evan Hensleigh <ehensleigh@toura.com>
2012-06-18 15:44:09 -0400

    Lookin' good.

--- app/toura/capabilities/Page_Audios_Playlist.js
+++ app/toura/capabilities/Page_Audios_Playlist.js
@@ -4,21 +4,44 @@ dojo.require('mulberry._Capability');
 
 dojo.declare('toura.capabilities.Page_Audios_Playlist', mulberry._Capability, {
   requirements : {
-    audioPlayer : 'AudioPlayer',
+    playlistButton : 'PlaylistButton',
     audioList : 'AudioList'
   },
 
   connects : [
     [ 'page', 'init', '_setup' ],
-    [ 'audioPlayer', '_playlistClick', 'foo' ]
+    [ 'playlistButton', 'onClick', 'togglePlaylist' ]
   ],
   
+  init : function() {
+    this.playlistVisible = false;
+    this.audioList.region.hide();
+  },
+
   _setup : function() {
     // debugger;
   },
   
-  foo: function() {
-    console.log("BAOOOEEAONU!");
-  }
+  togglePlaylist : function() {
+    if (this.playlistVisible) {
+      this._hidePlaylist.call(this);
+    } else {
+      this._showPlaylist.call(this);
+    }
+  },
+
+  _showPlaylist : function() {
+    this.playlistVisible = true;
+    this.playlistButton.addClass('playlist-visible');
+    this.audioList.region.show();
+  },
+
+  _hidePlaylist : function() {
+    this.playlistVisible = false;
+    this.playlistButton.removeClass('playlist-visible');
+    this.audioList.region.hide();
+  },
+
+  commaStopper : undefined
   
 });
