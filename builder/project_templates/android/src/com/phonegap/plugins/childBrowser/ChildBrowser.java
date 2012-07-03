ce5c20376dfedd4228356a02cb7ace2eef57f74d
Chris Barrett <christophersbarrett@gmail.com>
2012-07-03 14:27:18 -0500

    upgrading childbrowser to latest

--- builder/project_templates/android/src/com/phonegap/plugins/childBrowser/ChildBrowser.java
+++ builder/project_templates/android/src/com/phonegap/plugins/childBrowser/ChildBrowser.java
@@ -10,11 +10,13 @@ package com.phonegap.plugins.childBrowser;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.R;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -22,25 +24,28 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.text.InputType;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-
-import com.phonegap.api.PhonegapActivity;
-import com.phonegap.api.Plugin;
-import com.phonegap.api.PluginResult;
+import android.widget.RelativeLayout;
 
 public class ChildBrowser extends Plugin {
-
+    
     protected static final String LOG_TAG = "ChildBrowser";
     private static int CLOSE_EVENT = 0;
     private static int LOCATION_CHANGED_EVENT = 1;
@@ -49,7 +54,7 @@ public class ChildBrowser extends Plugin {
 
     private Dialog dialog;
     private WebView webview;
-    private EditText edittext;
+    private EditText edittext; 
     private boolean showLocationBar = true;
 
     /**
@@ -67,14 +72,14 @@ public class ChildBrowser extends Plugin {
         try {
             if (action.equals("showWebPage")) {
                 this.browserCallbackId = callbackId;
-
+                
                 // If the ChildBrowser is already open then throw an error
                 if (dialog != null && dialog.isShowing()) {
                     return new PluginResult(PluginResult.Status.ERROR, "ChildBrowser is already open");
                 }
-
+                
                 result = this.showWebPage(args.getString(0), args.optJSONObject(1));
-
+                
                 if (result.length() > 0) {
                     status = PluginResult.Status.ERROR;
                     return new PluginResult(status, result);
@@ -86,10 +91,10 @@ public class ChildBrowser extends Plugin {
             }
             else if (action.equals("close")) {
                 closeDialog();
-
+                
                 JSONObject obj = new JSONObject();
                 obj.put("type", CLOSE_EVENT);
-
+                
                 PluginResult pluginResult = new PluginResult(status, obj);
                 pluginResult.setKeepCallback(false);
                 return pluginResult;
@@ -172,24 +177,25 @@ public class ChildBrowser extends Plugin {
 
     /**
      * Navigate to the new page
-     *
+     * 
      * @param url to load
      */
-    private void navigate(String url) {
+    private void navigate(String url) {        
         InputMethodManager imm = (InputMethodManager)this.ctx.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(edittext.getWindowToken(), 0);
 
-        if (!url.startsWith("http")) {
+        if (!url.startsWith("http") && !url.startsWith("file:")) {
             this.webview.loadUrl("http://" + url);
+        } else {
+            this.webview.loadUrl(url);
         }
-        this.webview.loadUrl(url);
         this.webview.requestFocus();
     }
 
 
     /**
      * Should we show the location bar?
-     *
+     * 
      * @return boolean
      */
     private boolean getShowLocationBar() {
@@ -200,19 +206,34 @@ public class ChildBrowser extends Plugin {
      * Display a new browser with the specified URL.
      *
      * @param url           The url to load.
-     * @param jsonObject
+     * @param jsonObject 
      */
     public String showWebPage(final String url, JSONObject options) {
         // Determine if we should hide the location bar.
         if (options != null) {
             showLocationBar = options.optBoolean("showLocationBar", true);
         }
-
-        // Create dialog in new thread
+        
+        // Create dialog in new thread 
         Runnable runnable = new Runnable() {
+            /**
+             * Convert our DIP units to Pixels
+             * 
+             * @return int
+             */
+            private int dpToPixels(int dipValue) {
+                int value = (int) TypedValue.applyDimension( TypedValue.COMPLEX_UNIT_DIP,
+                                                            (float) dipValue,
+                                                            ctx.getContext().getResources().getDisplayMetrics()
+                );
+                
+                return value;
+            }
+            
             public void run() {
-                dialog = new Dialog(ctx.getContext(), android.R.style.Theme_Translucent_NoTitleBar);
-
+                // Let's create the main dialog
+                dialog = new Dialog(ctx.getContext(), android.R.style.Theme_NoTitleBar);
+                dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog.setCancelable(true);
                 dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
@@ -220,55 +241,80 @@ public class ChildBrowser extends Plugin {
                             try {
                                 JSONObject obj = new JSONObject();
                                 obj.put("type", CLOSE_EVENT);
-
+                                
                                 sendUpdate(obj, false);
                             } catch (JSONException e) {
                                 Log.d(LOG_TAG, "Should never happen");
                             }
                         }
                 });
-
-                LinearLayout.LayoutParams backParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-                LinearLayout.LayoutParams forwardParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-                LinearLayout.LayoutParams editParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT, 1.0f);
-                LinearLayout.LayoutParams closeParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-                LinearLayout.LayoutParams wvParams = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-
+                
+                // Main container layout
                 LinearLayout main = new LinearLayout(ctx.getContext());
                 main.setOrientation(LinearLayout.VERTICAL);
-
-                LinearLayout toolbar = new LinearLayout(ctx.getContext());
-                toolbar.setOrientation(LinearLayout.HORIZONTAL);
-
+                
+                // Toolbar layout
+                RelativeLayout toolbar = new RelativeLayout(ctx.getContext());
+                toolbar.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, this.dpToPixels(44)));
+                toolbar.setPadding(this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2));
+                toolbar.setHorizontalGravity(Gravity.LEFT);
+                toolbar.setVerticalGravity(Gravity.TOP);
+                
+                // Action Button Container layout
+                RelativeLayout actionButtonContainer = new RelativeLayout(ctx.getContext());
+                actionButtonContainer.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                actionButtonContainer.setHorizontalGravity(Gravity.LEFT);
+                actionButtonContainer.setVerticalGravity(Gravity.CENTER_VERTICAL);
+                actionButtonContainer.setId(1);
+                
+                // Back button
                 ImageButton back = new ImageButton(ctx.getContext());
-                back.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        goBack();
-                    }
-                });
-                back.setId(1);
+                RelativeLayout.LayoutParams backLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
+                backLayoutParams.addRule(RelativeLayout.ALIGN_LEFT);
+                back.setLayoutParams(backLayoutParams);
+                back.setContentDescription("Back Button");
+                back.setId(2);
                 try {
                     back.setImageBitmap(loadDrawable("www/childbrowser/icon_arrow_left.png"));
                 } catch (IOException e) {
                     Log.e(LOG_TAG, e.getMessage(), e);
                 }
-                back.setLayoutParams(backParams);
-
-                ImageButton forward = new ImageButton(ctx.getContext());
-                forward.setOnClickListener(new View.OnClickListener() {
+                back.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                        goForward();
+                        goBack();
                     }
                 });
-                forward.setId(2);
+
+                // Forward button
+                ImageButton forward = new ImageButton(ctx.getContext());
+                RelativeLayout.LayoutParams forwardLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
+                forwardLayoutParams.addRule(RelativeLayout.RIGHT_OF, 2);
+                forward.setLayoutParams(forwardLayoutParams);
+                forward.setContentDescription("Forward Button");
+                forward.setId(3);
                 try {
                     forward.setImageBitmap(loadDrawable("www/childbrowser/icon_arrow_right.png"));
                 } catch (IOException e) {
                     Log.e(LOG_TAG, e.getMessage(), e);
                 }
-                forward.setLayoutParams(forwardParams);
-
+                forward.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        goForward();
+                    }
+                });
+                
+                // Edit Text Box
                 edittext = new EditText(ctx.getContext());
+                RelativeLayout.LayoutParams textLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+                textLayoutParams.addRule(RelativeLayout.RIGHT_OF, 1);
+                textLayoutParams.addRule(RelativeLayout.LEFT_OF, 5);
+                edittext.setLayoutParams(textLayoutParams);
+                edittext.setId(4);
+                edittext.setSingleLine(true);
+                edittext.setText(url);
+                edittext.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+                edittext.setImeOptions(EditorInfo.IME_ACTION_GO);
+                edittext.setInputType(InputType.TYPE_NULL); // Will not except input... Makes the text NON-EDITABLE
                 edittext.setOnKeyListener(new View.OnKeyListener() {
                     public boolean onKey(View v, int keyCode, KeyEvent event) {
                         // If the event is a key-down event on the "enter" button
@@ -279,70 +325,84 @@ public class ChildBrowser extends Plugin {
                         return false;
                     }
                 });
-                edittext.setId(3);
-                edittext.setSingleLine(true);
-                edittext.setText(url);
-                edittext.setLayoutParams(editParams);
-
-                ImageButton close = new ImageButton((Context) ctx);
+                
+                // Close button
+                ImageButton close = new ImageButton(ctx.getContext());
+                RelativeLayout.LayoutParams closeLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
+                closeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                close.setLayoutParams(closeLayoutParams);
+                forward.setContentDescription("Close Button");
+                close.setId(5);
+                try {
+                    close.setImageBitmap(loadDrawable("www/childbrowser/icon_close.png"));
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                }   
                 close.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                         closeDialog();
                     }
                 });
-                close.setId(4);
-                try {
-                    close.setImageBitmap(loadDrawable("www/childbrowser/icon_close.png"));
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, e.getMessage(), e);
-                }
-                close.setLayoutParams(closeParams);
-
+                   
+                // WebView
                 webview = new WebView(ctx.getContext());
-                webview.getSettings().setJavaScriptEnabled(true);
-                webview.getSettings().setBuiltInZoomControls(true);
+                webview.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+                webview.setWebChromeClient(new WebChromeClient());
                 WebViewClient client = new ChildBrowserClient(edittext);
                 webview.setWebViewClient(client);
+                WebSettings settings = webview.getSettings();
+                settings.setJavaScriptEnabled(true);
+                settings.setJavaScriptCanOpenWindowsAutomatically(true);
+                settings.setBuiltInZoomControls(true);
+                settings.setPluginsEnabled(true);
+                settings.setDomStorageEnabled(true);
                 webview.loadUrl(url);
-                webview.setId(5);
-                webview.setInitialScale(0);
-                webview.setLayoutParams(wvParams);
+                webview.setId(6);
+                webview.getSettings().setLoadWithOverviewMode(true);
+                webview.getSettings().setUseWideViewPort(true);
                 webview.requestFocus();
-                webview.requestFocusFromTouch();
-
-
-                toolbar.addView(back);
-                toolbar.addView(forward);
+                webview.requestFocusFromTouch();   
+                
+                // Add the back and forward buttons to our action button container layout
+                actionButtonContainer.addView(back);
+                actionButtonContainer.addView(forward);
+                
+                // Add the views to our toolbar
+                toolbar.addView(actionButtonContainer);
                 toolbar.addView(edittext);
                 toolbar.addView(close);
-
+                
+                // Don't add the toolbar if its been disabled
                 if (getShowLocationBar()) {
+                    // Add our toolbar to our main view/layout
                     main.addView(toolbar);
                 }
+                
+                // Add our webview to our main view/layout
                 main.addView(webview);
 
                 WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
                 lp.copyFrom(dialog.getWindow().getAttributes());
                 lp.width = WindowManager.LayoutParams.FILL_PARENT;
                 lp.height = WindowManager.LayoutParams.FILL_PARENT;
-
+                
                 dialog.setContentView(main);
                 dialog.show();
                 dialog.getWindow().setAttributes(lp);
             }
 
-            private Bitmap loadDrawable(String filename) throws java.io.IOException {
-                InputStream input = ctx.getAssets().open(filename);
-                return BitmapFactory.decodeStream(input);
-            }
+          private Bitmap loadDrawable(String filename) throws java.io.IOException {
+              InputStream input = ctx.getAssets().open(filename);    
+              return BitmapFactory.decodeStream(input);
+          }
         };
         this.ctx.runOnUiThread(runnable);
         return "";
     }
-
+    
     /**
      * Create a new plugin result and send it back to JavaScript
-     *
+     * 
      * @param obj a JSONObject contain event payload information
      */
     private void sendUpdate(JSONObject obj, boolean keepCallback) {
@@ -361,39 +421,39 @@ public class ChildBrowser extends Plugin {
 
         /**
          * Constructor.
-         *
+         * 
          * @param mContext
-         * @param edittext
+         * @param edittext 
          */
         public ChildBrowserClient(EditText mEditText) {
             this.edittext = mEditText;
-        }
+        }       
 
         /**
          * Notify the host application that a page has started loading.
-         *
+         * 
          * @param view          The webview initiating the callback.
          * @param url           The url of the page.
          */
         @Override
         public void onPageStarted(WebView view, String url,  Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
+            super.onPageStarted(view, url, favicon);            
             String newloc;
-            if (url.startsWith("http:") || url.startsWith("https:")) {
+            if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("file:")) {
                 newloc = url;
             } else {
                 newloc = "http://" + url;
             }
-
-            if (!newloc.equals(edittext.getText().toString())) {
+            
+            if (!newloc.equals(edittext.getText().toString())) {           
                 edittext.setText(newloc);
             }
-
+            
             try {
                 JSONObject obj = new JSONObject();
                 obj.put("type", LOCATION_CHANGED_EVENT);
                 obj.put("location", url);
-
+                
                 sendUpdate(obj, true);
             } catch (JSONException e) {
                 Log.d("ChildBrowser", "This should never happen");
