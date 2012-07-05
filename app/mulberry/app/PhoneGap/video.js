2e442c021a4db9980a01e513a167bcb124cdb823
Chris Barrett <christophersbarrett@gmail.com>
2012-07-05 10:44:52 -0500

    Added video cordova plugin

---
+++ app/mulberry/app/PhoneGap/video.js
@@ -0,0 +1,37 @@
+dojo.provide('mulberry.app.PhoneGap.video');
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ *
+ * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2011, IBM Corporation
+ */
+mulberry.app.PhoneGap.video = function () {
+console.log("wtf");
+  (function(){
+  /**
+   * Constructor
+   */
+    function VideoPlayer() {
+      console.log('in VP constructor');
+    }
+
+    /**
+     * Starts the video player intent
+     *
+     * @param url           The url to play
+     */
+    VideoPlayer.prototype.play = function(url) {
+        cordova.exec(null, null, "VideoPlayer", "playVideo", [url]);
+    };
+
+    /**
+     * Load VideoPlayer
+     */
+    cordova.addConstructor(function() {
+        cordova.addPlugin("videoPlayer", new VideoPlayer());
+        console.log("in VP addconstructor");
+    });
+
+  }());
+};
