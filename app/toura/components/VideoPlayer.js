31a967b40bf95fa475b33d4e7d2f655590734947
Chris Barrett <christophersbarrett@gmail.com>
2012-07-06 10:09:09 -0500

    fixed a couple style complaints

--- app/toura/components/VideoPlayer.js
+++ app/toura/components/VideoPlayer.js
@@ -73,7 +73,7 @@ dojo.declare('toura.components.VideoPlayer', toura.components._MediaPlayer, {
     this.inherited(arguments);
 
     if (this.useHtml5Player) { return; }
-    if(mulberry.Device.environment == 'native'){
+    if (mulberry.Device.environment === 'native') {
       mulberry.app.PhoneGap.video.play(this.media.url);
     } else {
       mulberry.app.PhoneGap.browser.url(this.media.url);
