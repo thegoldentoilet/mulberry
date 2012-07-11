e641ba29b0f716333c1169831b28afcf2cfa7929
Chris Barrett <christophersbarrett@gmail.com>
2012-07-11 10:36:26 -0500

    commit changes to attempt to fix web links in native apps, totally doesn't work though. window.open just opens a new CB window instead of AB. This is being committed for posterity in case we revisit this.

--- app/mulberry/Device.js
+++ app/mulberry/Device.js
@@ -131,18 +131,33 @@ dojo.require('mulberry.app.Config');
        * 5. If Android, support for >= 2.2 (we can't test for improper
        *    implementation of background-size: contain/cover in 2.1)
        * */
-
+      
       var div = document.createElement('div'),
-          device = mulberry.Device,
-          supportsMulberry,
-          supportsWebSql,
-          supportsWebkitPrefixes,
-          isOldAndroid;
+        device = mulberry.Device,
+        supportsMulberry,
+        supportsWebSql,
+        supportsWebkitPrefixes,
+        isOldAndroid,
+        uri = window.location.href;
+        alert('uri: ' + uri);
+      if(uri.indexOf("?") > -1 && device.os === 'android') {
+          var href = uri.split("?");
+          alert('href: ' + href[1]);
+          var queryObject = dojo.queryToObject(href[1]);
+          alert('qo: '+queryObject.mby);
 
+        if(queryObject.mby === "1/") {
+          alert('opening in new');
+          alert(window.plugins);
+          //linking from native app to web app in android fails in childbrowser
+          //window.open(href[0]);
+          navigator.app.loadUrl(href[0], { openExternal:true } );
+        }
+      }
       if (device.environment != 'browser') {
         return true;
       }
-
+     
       if (window.location.hostname === 'localhost') {
         return true;
       }
--- app/mulberry/app/PhoneGap/browser.js
+++ app/mulberry/app/PhoneGap/browser.js
@@ -182,6 +182,7 @@ mulberry.app.PhoneGap.browser = function(pg, device){
     url : function(url) {
       if (pg) {
         if (os === 'android') {
+          url = url.indexOf("?") > -1 ? url+"&mby=1" : url+"?mby=1";
           cordova.exec(null, null, 'ChildBrowser', 'showWebPage', [url, false]);
         } else {
           window.plugins.childBrowser.showWebPage(url);
