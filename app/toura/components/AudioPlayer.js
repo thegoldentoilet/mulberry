12ea4e8960faa78b16669f4b181f822c2e997e95
Chris Barrett <christophersbarrett@gmail.com>
2012-07-16 14:28:56 -0500

    remove alerts

--- app/toura/components/AudioPlayer.js
+++ app/toura/components/AudioPlayer.js
@@ -155,11 +155,9 @@ dojo.declare('toura.components.AudioPlayer', toura.components._MediaPlayer, {
     if (this.useHtml5Player) { return; }
    
     if (!this.isPaused || (this.isPaused && !!media)) {
-      alert('play new song');
       this.player.play(this.media.url);
     } else if(this.isPaused && !media){
       this.set('isPaused', false);
-      alert('unpause song');
       this.player.play();
     }
   },
