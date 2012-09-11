a008414d16f546dd749ec28d4de6fe2b0389edd6
Evan Hensleigh <ehensleigh@toura.com>
2012-09-11 16:49:08 -0400

    Minor logic error in bootstrapping.
    App should wait for remote data to write.

--- app/mulberry/_Adapter.js
+++ app/mulberry/_Adapter.js
@@ -91,7 +91,7 @@ dojo.declare('mulberry._Adapter', null, {
    */
   _storeRemoteData : function(remoteData) {
     dojo.when(
-      this._store(remoteData),
+      this._store(remoteData, true),
       dojo.hitch(this, function() {
         // once we've stored it, we have a chance to run a hook
         this._onDataReady();
