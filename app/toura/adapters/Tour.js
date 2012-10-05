41e582a8377aa42641678f558c8aa73b451057bb
Evan Hensleigh <ehensleigh@toura.com>
2012-10-05 15:43:24 -0400

    BROKEN updateable merge still failing.

--- app/toura/adapters/Tour.js
+++ app/toura/adapters/Tour.js
@@ -1,8 +1,8 @@
 dojo.provide('toura.adapters.Tour');
 
-dojo.require('toura.adapters._Updateable');
+dojo.require('mulberry._Adapter');
 
-dojo.declare('toura.adapters.Tour', toura.adapters._Updateable, {
+dojo.declare('toura.adapters.Tour', mulberry._Adapter, {
   // this parser has to do very, very little
 
   appConfig : {},
@@ -14,6 +14,19 @@ dojo.declare('toura.adapters.Tour', toura.adapters._Updateable, {
 
   tableName : 'items',
 
+  /**
+   * The location of the bundled data, if any
+   * @optional
+   */
+  bundleDataUrl : '',
+
+  /**
+   * The location of the remote version information
+   * @required
+   */
+  remoteVersionUrl : '',
+
+
   constructor : function(config) {
     this.inherited(arguments);
     this.source = config && config.source || 'main';
@@ -21,17 +34,222 @@ dojo.declare('toura.adapters.Tour', toura.adapters._Updateable, {
     this.appConfig = mulberry.app.DeviceStorage.get(this.source + '-app');
   },
 
-  insertStatement : function(tableName, item) {
-    return [
-      "INSERT INTO " + tableName + " (id, json, source) VALUES ( ?, ?, ? )",
-      [ item.id, JSON.stringify(item), this.source ]
-    ];
+  /**************************/
+  /* BOOTSTRAP AND UPDATING */
+  /**************************/
+
+  /**
+   * @public
+   *
+   * This method ensures that bundled data has been loaded into the
+   * application, and then checks to see whether there is newer data available
+   * on the remote. If there is newer data available, then the local data is
+   * replaced with the newer data.
+   *
+   * @returns {Promise} A promise that, if resolved, will be resolved with a
+   * boolean value. If the value is true, then the data was updated during the
+   * bootstrapping process.
+   */
+  getData : function() {
+    var localVersion = this._getLocalVersion(),
+        dfd = this.deferred = new dojo.Deferred();
+
+    this.getBundleData().then(dojo.hitch(this, function(bundleData) {
+      var bundleVersion = bundleData.version,
+          initializeRequired = (localVersion === null) ||
+            (bundleVersion === null) ||
+            (localVersion < bundleVersion);
+
+      dojo.when(
+        initializeRequired ? this._initializeData(bundleData) : true,
+        dojo.hitch(this, '_updateIfNecessary')
+      ).then(dojo.hitch(this, '_onUpdate'));
+    }), dojo.hitch(this, function() {
+      dojo.hitch(this, '_updateIfNecessary').then(
+        dojo.hitch(this, '_onUpdate')
+      );
+    }));
+
+    return dfd.promise;
   },
 
-  getRootNodes : function() {
-    if (!this.appConfig || !this.appConfig.homeNodeId) { return; }
+  _updateIfNecessary : function() {
+    // update the local version if it was originally < 0
+    var dfd = new dojo.Deferred(),
+        localVersion = this._getLocalVersion();
 
-    return toura.Data.getModel(this.appConfig.homeNodeId, 'node').children;
+    dojo.when(
+      // first, get the remote version
+      this._getRemoteVersion(),
+
+      // once we have the remote version ...
+      dojo.hitch(this, function(remoteVersionData) {
+        var appMajorVersion = this._parseMajorVersion(mulberry.app.Config.get('appVersion'));
+
+        if (remoteVersionData) {
+          remoteVersionData.appVersion = remoteVersionData.appVersion || "2.0";
+        }
+
+        // check that app and remote major versions are compatible and that
+        // remote version is newer than the local version or local version is null.
+        if (
+           remoteVersionData &&
+           this._isAppVersionCompatible(remoteVersionData.appVersion) &&
+           (remoteVersionData.version > localVersion)
+        ) {
+
+          // if the remote version is newer, we need to fetch new data from the remote
+          this._getRemoteData()
+            .then(
+              // once we have new remote data ...
+              dojo.hitch(this, function(remoteData) {
+                if (!remoteData) {
+                  // if it was empty for some reason, we're done
+                  dfd.resolve(false);
+                  return;
+                }
+                dfd.resolve(remoteData);
+              })
+            );
+        } else {
+          // if the remote version isn't newer, the local data we have is fine
+
+          // again, we have a chance to run a hook if we need
+          this._onDataReady();
+
+          // since we didn't update the data, we resolve the dfd false
+          dfd.resolve(false);
+        }
+      }),
+
+      // ... hm, we did not get the remote version
+      dojo.hitch(this, function(error) {
+        // 404 or 503 OK, proceed as if there is no update
+        if (error.status === 404 || error.status === 503) {
+          // again, we have a chance to run a hook if we need
+          this._onDataReady();
+        }
+        dfd.resolve(false);
+      })
+    );
+
+    return dfd.promise;
+  },
+
+  /**********************/
+  /* VERSION MANAGEMENT */
+  /**********************/
+
+  /**
+   * @private
+   * @returns {Number} The current local version, or -1 if there is no current
+   * local version.
+   */
+  _getLocalVersion : function() {
+    var v = this.version || mulberry.app.DeviceStorage.get(this.source + '-version');
+    return v;
+  },
+
+  /**
+   * @private
+   */
+  _setLocalVersion : function(v) {
+    mulberry.app.DeviceStorage.set(this.source + '-version', v);
+  },
+
+  /**
+   * @private
+   * @returns {Promise} A promise that, if resolved, will be resolved with the
+   * current version of the remote data.
+   */
+  _getRemoteVersion : function() {
+    this.lastChecked = new Date().getTime();
+
+    var dfd = new dojo.Deferred();
+
+    if (mulberry.skipVersionCheck || !this.remoteVersionUrl) {
+      dfd.resolve(null);
+    } else {
+      mulberry.app.PhoneGap.network.isReachable()
+        .then(
+          dojo.hitch(this, function(isReachable) {
+            if (!isReachable) {
+              dfd.resolve(null);
+              return;
+            }
+
+            this._xhr(this.remoteVersionUrl, dfd);
+          }),
+
+          dfd.reject
+        );
+    }
+
+    return dfd.promise;
+  },
+
+  /**
+   * @private
+   *
+   * Parse the major version out of a version string as a number.
+   */
+  _parseMajorVersion: function(versionString) {
+    return +versionString.split('.')[0];
+  },
+
+  _isAppVersionCompatible: function(versionString) {
+    var appVersion = mulberry.app.Config.get('appVersion'),
+        appMajorVersion = this._parseMajorVersion(appVersion);
+
+    return appMajorVersion === this._parseMajorVersion(versionString);
+  },
+
+
+  /*******************/
+  /* DATA MANAGEMENT */
+  /*******************/
+
+  /**
+   * @private
+   * @returns {Object} A promise which resolves with the bundled data. If
+   *                     this is not a blessed tour, the tour should have
+   *                     no bundled data, so the promise is rejected.
+   */
+  getBundleData : function() {
+    var dfd = new dojo.Deferred();
+    if(this.blessed) {
+      dfd.resolve(toura.data.local);
+    } else {
+      dfd.reject();
+    }
+    return dfd.promise;
+  },
+
+  /**
+   * @private
+   *
+   * This method loads and stores the bundled data.
+   *
+   * @returns {Promise}
+   */
+  _initializeData : function(bundleData) {
+    var dfd = new dojo.Deferred();
+
+    dojo.when(bundleData || this.getBundleData(), dojo.hitch(this, function(data) {
+
+      if (!data) {
+        dfd.resolve(false);
+        return;
+      }
+
+      this._processData(data);
+
+      dojo.when(this._store(data), function() {
+        dfd.resolve(true);
+      });
+    }));
+
+    return dfd.promise;
   },
 
   _processData : function(data) {
@@ -44,16 +262,30 @@ dojo.declare('toura.adapters.Tour', toura.adapters._Updateable, {
       // TODO: figure out why this.inherited fails here
       this._items = data.items;
     }
+
+    if (data.version) {
+      this.version = data.version;
+    }
+  },
+
+  _storeRemoteData : function(remoteData) {
+    if (remoteData.appVersion && this._isAppVersionCompatible(remoteData.appVersion)) {
+      this.inherited(arguments);
+    } else {
+      this.deferred.resolve(false);
+    }
   },
 
   _store : function(newRemoteData) {
-    // TODO: setLocalVersion? does this get called in the process
-    // of updating + test case?
     var dfd = new dojo.Deferred(),
         storeOnDevice;
 
     if (this._items) {
-      storeOnDevice = mulberry.app.DeviceStorage.set(this.source, this._items);
+      storeOnDevice = mulberry.app.DeviceStorage.set(this.source, this._items, this);
+
+      storeOnDevice.then(dojo.hitch(this, function() {
+        this._setLocalVersion(this.version);
+      }));
 
       if (newRemoteData) {
         // if what we're storing is new remote data, then we should
@@ -69,16 +301,26 @@ dojo.declare('toura.adapters.Tour', toura.adapters._Updateable, {
     return dfd.promise;
   },
 
-  getBundleData : function() {
-    var dfd = new dojo.Deferred();
-    dfd.resolve(toura.data.local);
-    return dfd.promise;
-  },
-
   _onDataReady : function() {
     this.inherited(arguments);
     if (this.blessed) {
       mulberry.app.Config.set('app', this.appConfig);
     }
+  },
+
+
+
+
+  insertStatement : function(tableName, item) {
+    return [
+      "INSERT INTO " + tableName + " (id, json, source) VALUES ( ?, ?, ? )",
+      [ item.id, JSON.stringify(item), this.source ]
+    ];
+  },
+
+  getRootNodes : function() {
+    if (!this.appConfig || !this.appConfig.homeNodeId) { return; }
+
+    return toura.Data.getModel(this.appConfig.homeNodeId, 'node').children;
   }
 });
--- spec/app/toura/adapter_tour_spec.js
+++ spec/app/toura/adapter_tour_spec.js
@@ -58,43 +58,324 @@ describe('toura.adapters.Tour', function() {
       remoteDataUrl : 'remote',
       remoteVersionUrl : 'version',
       storageKey : 'key',
-      tableName : tableName
+      tableName : tableName,
+      source : 'foo',
+      blessed: true
     };
 
     networkIsReachable = true;
 
-    t = new toura.adapters.Tour(config);
-    mulberry.app.DeviceStorage.set(t.source, null, t);
+    Tour = toura.adapters.Tour;
   });
 
-  describe("getItems", function() {
-    it("should return a promise", function() {
-      var flag, bootstrap;
+  describe("bootstrapping", function() {
+    it("should indicate when the remote was last checked", function() {
+      var u = new Tour(config),
+          dfd = new dojo.Deferred();
 
-      bootstrap = t.getData();
+      expect(u.lastChecked).toBeDefined();
+      u.getData().then(dfd.resolve);
 
-      bootstrap.then(function() { flag = true; });
-
-      waitsFor(function() { return flag; });
+      waitsFor(function() { return dfd.fired; });
 
       runs(function() {
-         expect(t.getItems().then).toBeDefined();
+        expect(u.lastChecked).toBeTruthy();
+      });
+    });
+
+    describe("when the bundled data is newer than the stored data", function() {
+      var newerBundleData;
+
+      beforeEach(function() {
+        newerBundleData = dojo.mixin({}, toura.data.local);
+        newerBundleData.version = toura.data.local.version + 1;
+        newerBundleData.items = [ { id : 'new bundle' } ];
+      });
+
+      it("should replace the stored data with the bundled data", function() {
+        window.localStorage.clear();
+        networkIsReachable = false;
+
+        var t = new Tour(config), simulatePreviousBoot = t.getData(),
+            flag;
+
+        simulatePreviousBoot.then(function() {
+          flag = true;
+          ajaxMocks.bundle = newerBundleData;
+        });
+
+        waitsFor(function() { return flag; });
+
+        runs(function() {
+          var u = new Tour(config), dfd, flag;
+
+          u.getBundleData = function() {
+            var dfd = new dojo.Deferred();
+            dfd.resolve(newerBundleData);
+            return dfd.promise;
+          };
+          dfd = u.getData();
+
+          dfd.then(function() { flag = true; });
+
+          waitsFor(function() { return flag; });
+
+          runs(function() {
+            u.getItems().then(function(result) {
+              expect(result.length).toBe(1);
+              expect(result[0].id).toBe(newerBundleData.items[0].id);
+            });
+          });
+        });
+      });
+    });
+
+    describe("when the device is not connected to a network", function() {
+      beforeEach(function() {
+        networkIsReachable = false;
+      });
+
+      it("should resolve the deferred with a falsy value", function() {
+        var u = new Tour(config),
+            dfd = u.getData(),
+            flag, bootstrapperResult;
+
+        dfd.then(function(result) {
+          flag = true;
+          bootstrapperResult = result;
+        });
+
+        waitsFor(function() { return flag; }, 1000);
+
+        runs(function() {
+          expect(flag).toBeTruthy();
+          expect(bootstrapperResult).toBeFalsy();
+        });
+      });
+
+      it("should not try to contact the remote", function() {
+        var u = new Tour(config),
+            dfd = u.getData(),
+            spy = spyOn(dojo, 'xhrGet'),
+            flag;
+
+        dfd.then(function(result) {
+          flag = true;
+        });
+
+        waitsFor(function() { return flag; }, 1000);
+
+        runs(function() {
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe("when the device is connected to a network", function() {
+      beforeEach(function() {
+        networkIsReachable = true;
+      });
+
+      function itShouldLoadTheRemoteData() {
+
+        it("should store the remote data in memory", function() {
+          var u = new Tour(config),
+              dfd = u.getData(),
+              flag;
+
+          dfd.then(function(result) {
+            flag = true;
+          });
+
+          waitsFor(function(result) {
+            return flag;
+          });
+
+          runs(function() {
+            u.getItems().then(function(result) {
+              expect(result.length).toBe(1);
+              expect(result[0].id).toBe(newerRemoteData.items[0].id);
+            });
+          });
+        });
+
+        it("should resolve the deferred true", function() {
+          var u = new Tour(config),
+              dfd = u.getData(),
+              bootstrapperResult;
+
+          dfd.then(function(result) {
+            bootstrapperResult = result;
+          });
+
+          waitsFor(function() {
+            return bootstrapperResult;
+          });
+
+          runs(function() {
+            expect(bootstrapperResult).toBeTruthy();
+          });
+        });
+
+        it("should run the _store method, which can be implemented by subclasses", function() {
+          var u = Tour(config),
+              spy = spyOn(u, '_store'),
+              dfd = u.getData(),
+              flag;
+
+          dfd.then(function() {
+            flag = true;
+          });
+
+          waitsFor(function() { return flag; });
+
+          runs(function() {
+            expect(spy).toHaveBeenCalled();
+          });
+        });
+
+      }
+
+      function itShouldNotLoadTheRemoteData() {
+
+        it("should not load the remote data", function() {
+          var u = new Tour(config),
+              spy = spyOn(u, '_getRemoteData'),
+              dfd = u.getData(),
+              flag;
+
+          dfd.then(function() {
+            flag = true;
+          });
+
+          waitsFor(function() {
+            return flag;
+          });
+
+          runs(function() {
+            expect(spy).not.toHaveBeenCalled();
+          });
+        });
+
+        it("should resolve the deferred false", function() {
+          var u = new Tour(config),
+              dfd = u.getData(),
+              bootstrapperResult,
+              flag;
+
+          dfd.then(function(result) {
+            flag = true;
+            bootstrapperResult = result;
+          });
+
+          waitsFor(function() {
+            return flag;
+          });
+
+          runs(function() {
+            expect(bootstrapperResult).toBeFalsy();
+          });
+        });
+
+      }
+
+      describe("and there is newer data", function() {
+        beforeEach(function() {
+          ajaxMocks.version.version  = toura.data.local.version + 100;
+        });
+
+        describe("and the remote app version is the same", function() {
+          itShouldLoadTheRemoteData();
+        });
+
+        describe("and the remote app version is smaller", function() {
+          beforeEach(function() {
+            ajaxMocks.version.appVersion =  (appMajorVersion-1) + ".0";
+          });
+          itShouldNotLoadTheRemoteData();
+        });
+
+        describe("and the remote app version is greater", function() {
+          beforeEach(function() {
+            ajaxMocks.version.appVersion =  (appMajorVersion+1) + ".0";
+          });
+          itShouldNotLoadTheRemoteData();
+        });
+
+        describe("and the data has a newer app version", function() {
+          beforeEach(function() {
+            ajaxMocks.remote.appVersion = (appMajorVersion+1) + ".0";
+          });
+
+          it("should not store the remote data", function() {
+            var u = new Tour(config),
+                spy = spyOn(u, '_store'),
+                dfd = u.getData(),
+                flag;
+
+            dfd.then(function() {
+              flag = true;
+            });
+
+            waitsFor(function() {
+              return flag;
+            });
+
+            runs(function() {
+              expect(spy).not.toHaveBeenCalled();
+            });
+          });
+
+        });
+
+      });
+
+      describe("and there is not newer data", function() {
+        beforeEach(function() {
+          ajaxMocks.version = { version : 0 };
+        });
+
+        itShouldNotLoadTheRemoteData();
+
       });
     });
   });
 
-  describe("getRootNodes", function() {
-    it("should get the children of the home node", function() {
-      var flag, bootstrap;
+  describe("getters", function() {
+    beforeEach(function() {
+      t = new Tour(config);
+      mulberry.app.DeviceStorage.set(t.source, null, t);
+    });
 
-      bootstrap = t.getData();
+    describe("getItems", function() {
+      it("should return a promise", function() {
+        var flag, bootstrap;
 
-      bootstrap.then(function() { flag = true; });
+        bootstrap = t.getData();
 
-      waitsFor(function() { return flag; });
+        bootstrap.then(function() { flag = true; });
 
-      runs(function() {
-        expect(t.getRootNodes()).toEqual(toura.Data.getModel('node-home').children);
+        waitsFor(function() { return flag; });
+
+        runs(function() {
+           expect(t.getItems().then).toBeDefined();
+        });
+      });
+    });
+
+    describe("getRootNodes", function() {
+      it("should get the children of the home node", function() {
+        var flag, bootstrap;
+
+        bootstrap = t.getData();
+
+        bootstrap.then(function() { flag = true; });
+
+        waitsFor(function() { return flag; });
+
+        runs(function() {
+          expect(t.getRootNodes()).toEqual(toura.Data.getModel('node-home').children);
+        });
       });
     });
   });
