ef2b587dc2cd347d067cfc8c3aced4068faadb78
Evan Hensleigh <ehensleigh@toura.com>
2012-10-08 12:31:29 -0400

    Missed updates to names in Bootstrapper.

--- app/toura/Bootstrapper.js
+++ app/toura/Bootstrapper.js
@@ -2,7 +2,7 @@ dojo.provide('toura.Bootstrapper');
 
 dojo.require('mulberry.app.PhoneGap');
 dojo.require('mulberry.app.DeviceStorage');
-dojo.require('toura.adapters.tourjs');
+dojo.require('toura.adapters.Tour');
 
 dojo.requireLocalization('mulberry', 'mulberry');
 
@@ -17,7 +17,7 @@ var bootstrapper = function() {
     app.DeviceStorage.set('tour-version', null);
   }
 
-  tour = new toura.adapters.tourjs({
+  tour = new toura.adapters.Tour({
     remoteDataUrl : app.Config.get('updateUrl'),
     remoteVersionUrl : app.Config.get('versionUrl'),
     source : 'main',
@@ -39,7 +39,7 @@ var bootstrapper = function() {
 
       if (!outdated) { return; }
 
-      tour.bootstrap().then(function(gotUpdate) {
+      tour.getData().then(function(gotUpdate) {
         if (!gotUpdate) { return; }
 
         dojo.when(tour.getItems(), function(data) {
@@ -57,7 +57,7 @@ var bootstrapper = function() {
     });
   }
 
-  tour.bootstrap().then(function() {
+  tour.getData().then(function() {
     dfd.resolve(tour);
   }, dfd.reject);
 
