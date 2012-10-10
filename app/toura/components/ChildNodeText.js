c6c62c346e24208c26ab1036eb8a92b70600788f
Evan Hensleigh <ehensleigh@toura.com>
2012-10-10 16:47:44 -0400

    ChildNodeText updated.

--- app/toura/components/ChildNodeText.js
+++ app/toura/components/ChildNodeText.js
@@ -4,5 +4,6 @@ dojo.require('toura.components.ChildNodes');
 
 dojo.declare('toura.components.ChildNodeText', toura.components.ChildNodes, {
   templateString : dojo.cache('toura.components', 'ChildNodeText/ChildNodeText.haml'),
+  itemTemplate : Haml(dojo.cache('toura.components', 'ChildNodeText/ChildNodeTextItem.haml')),
   handleClicks : false
 });
